a47f5a7fdedead687d2332d1ec05028bbd904ddf
tsugehara <tsugehara@gmail.com>
2020-08-12 21:16:35 +0900

    eslint入れた

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: [
+		"airbnb-typescript",
+	],
+	parserOptions: {
+		project: "./tsconfig.json",
+	},
+	rules: {
+		// consoleは使っていい
+		"no-console": "off",
+		// クォートはダブルクォート
+		"@typescript-eslint/quotes": ["error", "double"],
+		// インデントはタブ
+		"@typescript-eslint/indent": ["error", "tab"],
+		// 行辺りの文字数は80じゃなくて180
+		"max-len": ["error", { code: 160 }],
+		// インデントにタブは使っていい（途中のタブは駄目）
+		"no-tabs": ["error", { allowIndentationTabs: true }],
+		// インデントはタブ！
+		"react/jsx-indent": ["error", "tab"],
+		// インデントはタブ！！
+		"react/jsx-indent-props": ["error", "tab"],
+	}
+};
+
